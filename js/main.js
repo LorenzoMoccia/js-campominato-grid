2b40e0e1ebff11cace82a966400b1793fade4d90
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-03 11:58:54 +0100

    Ciclo for, creazione elemento div, aggiunta classe

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const boardContainer = document.querySelector(".board");
+
+for(let i = 1; i <= 100; i++){
+    const boardCell = document.createElement("div");
+    boardCell.innerHTML = i;
+    boardCell.classList.add("board__number");
+    boardContainer.append(boardCell);
+}
