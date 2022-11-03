4035a4f4c772db672ae779a48d4910db06812b6a
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-03 12:13:45 +0100

    Terminato

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const boardContainer = document.querySelector(".board");
 for(let i = 1; i <= 100; i++){
     const boardCell = document.createElement("div");
     boardCell.innerHTML = i;
-    boardCell.classList.add("board__number");
+    boardCell.classList.add("board-number");
+
+    //Evento click
+    boardCell.addEventListener("click", function(){
+        
+        this.classList.add("cell-color");
+        console.log(this.innerHTML);
+    })
+
     boardContainer.append(boardCell);
 }
